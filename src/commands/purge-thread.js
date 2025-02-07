1fb4f1d1631963c18c3105c8cc63098af944e07e
King <47607185+DarkGamerYT@users.noreply.github.com>
2025-02-07 23:31:41 +0100

    Improvements

--- src/commands/purge-thread.js
+++ src/commands/purge-thread.js
@@ -28,7 +28,7 @@ export default {
     async execute(interaction) {
         /** @type { import("discord.js").ThreadChannel } */
         const post = interaction.options.getChannel("thread");
-        if (post === void 0 || post.type !== ChannelType.ThreadChannel) {
+        if (post === void 0 || post.type !== ChannelType.PublicThread) {
             await interaction.reply({ content: "Channel is not a thread or does not exist.", ephemeral: true });
             return;
         };
@@ -67,13 +67,15 @@ export default {
         };
 
         console.log(`(${members.length})`, members.map((user) => user.username));
-        await interaction.editReply({
-            content: `\`${members.length}\` members have sent at least one message in the last month.`
-        });
-        
         const postMembers = await post.members.fetch({ withMember: true });
+        await interaction.editReply({
+            content: `\`${members.length}\` member(s) have sent at least one message in the last month in ${post.toString()}.`
+        });
+
         await post.setLocked(true, "Purging members");
-        for (let i = 0; i < 75; i++) {
+
+        let amount = 0;
+        for (let i = 0; i < postMembers.size; i++) {
             const member = postMembers.at(i);
             if (member === void 0)
                 break;
@@ -85,15 +87,20 @@ export default {
 
             try {
                 await post.members.remove(member.id, "Purging members");
+                amount++;
             } catch {
-                console.log("Failed to remove", member)
+                console.log("Failed to remove", member);
             };
+
+            // Sleep to hopefully avoid any ratelimits
+            if (amount % 75 == 0)
+                await new Promise((resolve) => setTimeout(resolve, 1 * 60 * 1000));
         };
 
         console.log("Done purging!");
         await post.setLocked(false, "Purging members");
         await interaction.editReply({
-            content: `Done purging!\n\`${members.length}\` members have sent at least one message in the last month.`
+            content: `\`${members.length}\` member(s) have sent at least one message in the last month in ${post.toString()}.\nDone purging! Removed \`${amount}\` member(s).`
         });
     },
 };
